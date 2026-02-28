a0355755ac7248071facde1014f6fc9dc849f6a6
Ratul Raha <gkdratul@gmail.com>
2026-02-28 23:05:59 +0600

    fixed baseurl for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  // baseURL: "http://localhost:5000/api", // backend URL
+  baseUrl: "https://easyurl-server.vercel.app/api", // production URL
   withCredentials: true, // for cookies later
 })
 
