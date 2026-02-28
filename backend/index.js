cbd2d913ef96b33b97b2a6b901a8b6db15dc037f
Ratul Raha <gkdratul@gmail.com>
2026-02-28 23:50:46 +0600

    fixed origin url

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: "https://easyurl-client.vercel.app/",
+    origin: "https://easyurl-client.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
